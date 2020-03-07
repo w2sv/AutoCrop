87211ab9fae57cdc77ef4b12cdeb2e2891975832
Janek Zangenberg <zangenbergjanek@googlemail.com>
2020-03-07 11:22:43 +0100

    recreation after cropping

--- app/src/main/java/com/example/screenshotboundremoval/MainActivity.kt
+++ app/src/main/java/com/example/screenshotboundremoval/MainActivity.kt
@@ -10,15 +10,10 @@ import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.display_screen.*
 import java.io.InputStream
-
-import java.io.File
-import java.io.FileOutputStream
 
 //TODO: loading screen, exception handling, welcome screen, selection screen pimp, saving
 
@@ -32,21 +27,7 @@ class MainActivity : AppCompatActivity() {
     private val permission2Code: Map<String, Int> = mapOf(
         Manifest.permission.WRITE_EXTERNAL_STORAGE to WRITE_PERMISSION_CODE,
         Manifest.permission.READ_EXTERNAL_STORAGE to READ_PERMISSION_CODE)
-    var requiredPermissions: Int = 0
-
-    private fun checkPermission(permission: String){
-        if (checkSelfPermission(permission) == PackageManager.PERMISSION_DENIED){
-            requestPermissions(arrayOf(permission), permission2Code[permission]!!)
-            requiredPermissions ++
-        }
-    }
-
-    private fun requestActivityPermissions(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-        }
-    }
+    private var requiredPermissions: Int = 0
 
     /*
      * invoked when activity created; resets previous states, initializes screen, requests permissions
@@ -63,11 +44,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun permissionRequestResultHandling(grantResults: IntArray, requestDescription: String){
-        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_DENIED)
-            Toast.makeText(this, "$requestDescription permission denied", Toast.LENGTH_SHORT).show()
-        else
-            requiredPermissions --
+    // ----------------
+    // PERMISSION QUERY
+    // ----------------
+
+    private fun checkPermission(permission: String){
+        if (checkSelfPermission(permission) == PackageManager.PERMISSION_DENIED){
+            requestPermissions(arrayOf(permission), permission2Code[permission]!!)
+            requiredPermissions ++
+        }
+    }
+
+    private fun requestActivityPermissions(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -76,6 +68,17 @@ class MainActivity : AppCompatActivity() {
             WRITE_PERMISSION_CODE -> permissionRequestResultHandling(grantResults, "Write")
         }
     }
+
+    private fun permissionRequestResultHandling(grantResults: IntArray, requestDescription: String){
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_DENIED)
+            Toast.makeText(this, "$requestDescription permission denied", Toast.LENGTH_SHORT).show()
+        else
+            requiredPermissions --
+    }
+
+    // ----------------
+    // IMAGE SELECTION
+    // ----------------
 
     private fun pickImageFromGallery() {
         val intent = Intent(Intent.ACTION_PICK)
@@ -94,28 +97,16 @@ class MainActivity : AppCompatActivity() {
             val cropper = Cropper(bitmap)
             val croppedBitmap: Bitmap = cropper.getCroppedBitmap()
 
+            // display cropped Image
             setContentView(R.layout.display_screen)
             image_view.setImageBitmap(croppedBitmap)
 
-            this.recreate()
-
-            // saving new image
-//            val path: File = Environment.getExternalStorageDirectory()
-//            val newFile = File(path ,"cropped.png")
-//
-//            println("post creation")
-//
-//            val fileOut = FileOutputStream(newFile)
-//
-//            println("Foo")
-//
-//            croppedBitmap.compress(Bitmap.CompressFormat.PNG, 20, fileOut)
-//            fileOut.flush()
-//            fileOut.close()
-//
-//            println("Foo1")
-//            MediaStore.Images.Media.insertImage(contentResolver, croppedBitmap, newFile.absolutePath, newFile.absolutePath)
-
+            // procedure
+            /*val procedureDialog = ProcedureDialog()
+            procedureDialog.show(supportFragmentManager, "procedure dialog")
+         */
+            // repeat
+            // this.recreate()
         }
     }
 }
--- app/src/main/java/com/example/screenshotboundremoval/ProcedureDialog.kt
+++ app/src/main/java/com/example/screenshotboundremoval/ProcedureDialog.kt
@@ -3,22 +3,52 @@ package com.example.screenshotboundremoval
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatDialogFragment
+import java.io.File
+import java.io.FileOutputStream
 
-class DialogOnClickListener: DialogInterface.OnClickListener{
-    override fun onClick(dialog: DialogInterface?, which: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
 
 class ProcedureDialog : AppCompatDialogFragment(){
+    private class SaveButtonOnClickListener: DialogInterface.OnClickListener{
+        /*private fun saveImage(){
+            val path: File = Environment.getExternalStorageDirectory()
+            val newFile = File(path ,"cropped.png")
+
+            println("post creation")
+
+            val fileOut = FileOutputStream(newFile)
+
+            println("Foo")
+
+            croppedBitmap.compress(Bitmap.CompressFormat.PNG, 20, fileOut)
+            fileOut.flush()
+            fileOut.close()
+
+            println("Foo1")
+            MediaStore.Images.Media.insertImage(contentResolver, croppedBitmap, newFile.absolutePath, newFile.absolutePath)
+        }*/
+        override fun onClick(dialog: DialogInterface?, which: Int) {
+            // saveImage()
+        }
+    }
+
+    private class DiscardButtonOnClickListener: DialogInterface.OnClickListener{
+        override fun onClick(dialog: DialogInterface?, which: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(this.activity)
         builder
             .setTitle("Title")
             .setMessage("How do you want to proceed?")
-            .setPositiveButton("save", DialogOnClickListener())
+            .setPositiveButton("Save", SaveButtonOnClickListener())
+            .setNegativeButton("Discard", DiscardButtonOnClickListener())
 
         return builder.create()
     }
