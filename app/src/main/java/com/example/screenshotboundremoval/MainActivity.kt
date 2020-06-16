8ad43e57fff10683c36511060a28f67dbee27948
Janek Zangenberg <zangenbergjanek@googlemail.com>
2020-06-16 15:12:55 +0200

    working orientation retrieval

--- app/src/main/java/com/example/screenshotboundremoval/MainActivity.kt
+++ app/src/main/java/com/example/screenshotboundremoval/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.screenshotboundremoval
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -14,30 +19,27 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-//TODO: welcome screen, selection screen pimp, Logo
+//TODO: welcome screen, image selection screen pimp, Logo
 
 const val N_DISMISSED_IMAGES = "com.example.screenshotboundremoval.N_DISMISSED_IMAGES"
 const val DISMISSED_ALL_IMAGES = "com.example.screenshotboundremoval.DISMISSED_ALL_IMAGES"
 const val ATTEMPTED_FOR_MULTIPLE_IMAGES = "com.example.screenshotboundremoval.ATTEMPTED_FOR_MULTIPLE_IMAGES"
 
-class MainActivity : AppCompatActivity() {
-    companion object{
-        private const val IMAGE_PICK_CODE = 69
-        private const val READ_PERMISSION_CODE = 420
-        private const val WRITE_PERMISSION_CODE = 47
-    }
 
-    private val permission2Code: Map<String, Int> = mapOf(
-        Manifest.permission.WRITE_EXTERNAL_STORAGE to WRITE_PERMISSION_CODE,
-        Manifest.permission.READ_EXTERNAL_STORAGE to READ_PERMISSION_CODE
-    )
+class MainActivity : Activity(), SensorEventListener {
 
-    private var nRequiredPermissions: Int = 0
+    private lateinit var sensorManager: SensorManager
+    private val accelerometerReading = FloatArray(3)
+    private val magnetometerReading = FloatArray(3)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private val rotationMatrix = FloatArray(9)
+    private val orientationAngles = FloatArray(3)
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
         // procedure result display if existent
         if (intent.getBooleanExtra(DISMISSED_ALL_IMAGES, false))
@@ -61,6 +63,88 @@ class MainActivity : AppCompatActivity() {
                 pickImageFromGallery()
         }
     }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Do something here if sensor accuracy changes.
+        // You must implement this callback in your code.
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Get updates from the accelerometer and magnetometer at a constant rate.
+        // To make batch operations more efficient and reduce power consumption,
+        // provide support for delaying updates to the application.
+        //
+        // In this example, the sensor reporting delay is small enough such that
+        // the application receives an update before the system checks the sensor
+        // readings again.
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
+            sensorManager.registerListener(
+                this,
+                magneticField,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        // Don't receive any more updates from either sensor.
+        sensorManager.unregisterListener(this)
+    }
+
+    // Get readings from accelerometer and magnetometer. To simplify calculations,
+    // consider storing these readings as unit vectors.
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+        }
+        updateOrientationAngles()
+        println("YARP")
+        orientationAngles.forEach { print("$it\t") }
+    }
+
+    // Compute the three orientation angles based on the most recent readings from
+    // the device's accelerometer and magnetometer.
+    fun updateOrientationAngles() {
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(
+            rotationMatrix,
+            null,
+            accelerometerReading,
+            magnetometerReading
+        )
+
+        // "mRotationMatrix" now has up-to-date information.
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
+    }
+
+    // -------------------------------------------------------------------
+    companion object{
+        private const val IMAGE_PICK_CODE = 69
+        private const val READ_PERMISSION_CODE = 420
+        private const val WRITE_PERMISSION_CODE = 47
+    }
+
+    private val permission2Code: Map<String, Int> = mapOf(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE to WRITE_PERMISSION_CODE,
+        Manifest.permission.READ_EXTERNAL_STORAGE to READ_PERMISSION_CODE
+    )
+
+    private var nRequiredPermissions: Int = 0
 
     // ----------------
     // PERMISSION QUERY
