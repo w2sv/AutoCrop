448fa13323ec968eaef74d458e9f799d5f2f72c7
Janek Zangenberg <zangenbergjanek@googlemail.com>
2020-06-18 12:16:39 +0200

    pixel field acceleration

--- app/src/main/java/com/example/screenshotboundremoval/PixelField.java
+++ app/src/main/java/com/example/screenshotboundremoval/PixelField.java
@@ -3,12 +3,13 @@ package com.example.screenshotboundremoval;
 import processing.core.PApplet;
 
 public class PixelField extends PApplet {
-    static float xOffset = 0;
-    static float yOffset = 0;
-    float INC = (float)0.02;
-    float Z_INC = (float)0.08;
+    float INC = (float)0.1;
+    float Z_INC = (float)0.2;
 
     float z_off = 0;
+
+    int width_step = 4;
+    int height_step = 4;
 
     public void settings() {
         size(720, 1400, P2D);
@@ -20,11 +21,16 @@ public class PixelField extends PApplet {
     public void draw() {
         float y_off = 0;
         loadPixels();
-        for (int y = 0; y < height; y++){
+        for (int y = 0; y < height; y+=height_step){
             float x_off = 0;
-            for (int x = 0; x < width; x++){
+            for (int x = 0; x < width; x+=width_step){
                 float r = noise(x_off, y_off, z_off) * 127;
-                pixels[x + y * width] = color(r * 2, r / 2, r);
+
+                for (int x_i = 0; x_i < width_step; x_i++){
+                    for (int y_i = 0; y_i < height_step; y_i++){
+                        pixels[x + x_i + (y + y_i) * width] = color(r * 2, r / 2, r);
+                    }
+                }
                 x_off += INC;
             }
             y_off += INC;
