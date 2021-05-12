66dd81c13caed7717a9d32f00d6bfe7e81da42b7
W2SV <zangenbergjanek@googlemail.com>
2021-05-12 16:10:53 +0200

    Cryptic error

--- app/src/main/java/com/autocrop/activities/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/autocrop/activities/welcome/WelcomeActivity.kt
@@ -31,7 +31,7 @@ class WelcomeActivity : SystemUiHidingAppCompatActivity() {
                 restartTransitionAnimation()
                 finish()
             },
-            500  // TODO
+            1500
         )
     }
 }
