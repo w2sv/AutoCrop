fa1c76887748ef3a0ed5d132707582c9ba79cd67
W2SV <zangenbergjanek@googlemail.com>
2021-05-16 10:56:54 +0200

    Clean up main activity

--- app/src/main/java/com/autocrop/activities/main/Activity.kt
+++ app/src/main/java/com/autocrop/activities/main/Activity.kt
@@ -4,13 +4,7 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Environment
-import android.os.Handler
-import android.view.View
-import android.view.accessibility.AccessibilityEvent
-import android.widget.Button
 import android.widget.FrameLayout
-import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.widget.PopupMenu
 import com.autocrop.UserPreferences
@@ -21,10 +15,8 @@ import com.autocrop.utils.android.*
 import com.autocrop.utils.formattedDateTimeString
 import com.autocrop.utils.getByBoolean
 import com.bunsenbrenner.screenshotboundremoval.R
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import processing.android.PFragment
-import processing.core.PApplet
 import timber.log.Timber
 import java.io.File
 
@@ -35,188 +27,7 @@ val SELECTED_IMAGE_URI_STRINGS_IDENTIFIER: String =
 
 class MainActivity : SystemUiHidingFragmentActivity(R.layout.activity_main) {
 
-    companion object {
-
-        private enum class PermissionCode {
-            WRITE,
-            READ,
-            MULTIPLE;
-
-            companion object {
-                operator fun get(index: Int): PermissionCode = values()[index]
-            }
-        }
-
-        private val REQUIRED_PERMISSIONS: List<String> = listOf(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        )
-
-        private val PERMISSION_2_DESCRIPTION: Map<String, String> = listOf("writing", "reading")
-            .mapIndexed { index, s -> REQUIRED_PERMISSIONS[index] to s }
-            .toMap()
-
-        private val PERMISSION_2_CODE: Map<String, PermissionCode> =
-            REQUIRED_PERMISSIONS
-                .mapIndexed { index, s -> s to PermissionCode[index] }
-                .toMap()
-
-        private enum class IntentCode {
-            IMAGE_SELECTION
-        }
-
-        val MENU_ITEM_ID_2_PREFERENCE_KEY: Map<Int, String> = mapOf(
-            R.id.main_menu_item_delete_input_screenshots to UserPreferences.Keys.deleteInputScreenshots,
-            R.id.main_menu_item_save_to_autocropped_dir to UserPreferences.Keys.saveToAutocroppedDir,
-            R.id.main_menu_conduct_auto_scrolling to UserPreferences.Keys.conductAutoScrolling
-        )
-    }
-
-    // -----------------Permissions---------------------
-
-    private val permission2IsGranted: MutableMap<String, Boolean> by lazy {
-        REQUIRED_PERMISSIONS
-            .associateWith { permissionGranted(it) }
-            .toMutableMap()
-    }
-
-    private val allPermissionsGranted: Boolean
-        get() = permission2IsGranted.values.all { it }
-
-    private fun requestActivityPermissions() {
-        with(
-            REQUIRED_PERMISSIONS
-                .filter { permission2IsGranted[it] == false }
-                .toTypedArray()
-        ) {
-            requestPermissions(
-                this,
-                if (size > 1)
-                    PermissionCode.MULTIPLE.ordinal
-                else
-                    PERMISSION_2_CODE[get(0)]!!.ordinal
-            )
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        if (grantResults.all { it == PackageManager.PERMISSION_DENIED })
-            return displayToast(
-                "You need to permit file reading and\n" +
-                        "writing in order for the app to work"
-            )
-
-        (permissions zip grantResults.toTypedArray()).forEach {
-            if (it.second != PackageManager.PERMISSION_GRANTED)
-                displayToast(
-                    "You need to permit file ${PERMISSION_2_DESCRIPTION[it.first]}\n" +
-                            "in order for the app to work"
-                )
-            else
-                permission2IsGranted[it.first] = true
-        }
-
-        if (allPermissionsGranted)
-            selectImages()
-    }
-
-    // ------------Lifecycle stages---------------
-
-    private lateinit var flowFieldPApplet: FlowFieldPApplet
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        fun setPixelField() {
-            if (!::flowFieldPApplet.isInitialized)
-                flowFieldPApplet = FlowFieldPApplet(
-                    screenResolution(windowManager)
-                ).also {
-                    Timber.i("Initialized flowFieldPApplet")
-                }
-
-            PFragment(flowFieldPApplet).setView(
-                findViewById<FrameLayout>(R.id.canvas_container), this
-            )
-        }
-
-        fun setButtonOnClickListeners() {
-            // image selection button
-            image_selection_button.setOnClickListener {
-                if (!allPermissionsGranted)
-                    requestActivityPermissions()
-                else
-                    selectImages()
-            }
-
-            // menu button
-            menu_button.setOnClickListener {
-                // inflate popup menu
-                PopupMenu(this, it).run {
-                    menuInflater.inflate(R.menu.activity_main, menu)
-
-                    // set checks
-                    MENU_ITEM_ID_2_PREFERENCE_KEY.entries.forEach { (key, value) ->
-                        menu.findItem(key).isChecked = UserPreferences[value]!!
-                    }
-
-                    // set item onClickListeners
-                    setOnMenuItemClickListener { item ->
-                        item.run {
-                            UserPreferences.toggle(MENU_ITEM_ID_2_PREFERENCE_KEY[itemId]!!)
-                            isChecked = !isChecked
-                            persistMenuAfterItemClick(this)
-                        }
-                    }
-                    show()
-                }
-            }
-
-            // screenshot button
-            screenshot_button.setOnClickListener {
-                flowFieldPApplet.canvas.save(
-                    File(
-                        picturesDirectoryPath,
-                        "AutoCrop${formattedDateTimeString()}.jpg"
-                    ).absolutePath
-                        .also { Timber.i("Saving flowfield canvas to $it") }
-                )
-
-                displayToast(
-                    "Saved Flowfield Image to\n${picturesDirectoryPath}",
-                    TextColors.successfullyCarriedOut,
-                    Toast.LENGTH_SHORT
-                )
-            }
-        }
-
-        fun displaySavingResultSnackbar(nSavedCrops: Int) {
-            val textColor: Int = TextColors.successfullyCarriedOut
-
-            with(UserPreferences.deleteInputScreenshots) {
-                when (nSavedCrops) {
-                    0 -> displaySnackbar("Dismissed all crops", textColor)
-                    1 -> displaySnackbar(
-                        listOf(
-                            "Saved 1 crop",
-                            "Saved 1 crop and deleted\ncorresponding screenshot"
-                        ).getByBoolean(this),
-                        textColor
-                    )
-                    in 2..Int.MAX_VALUE -> displaySnackbar(
-                        listOf(
-                            "Saved $nSavedCrops crops",
-                            "Saved $nSavedCrops crops and deleted\ncorresponding screenshots"
-                        ).getByBoolean(this),
-                        textColor
-                    )
-                    else -> Unit
-                }
-            }
-        }
-
         super.onCreate(savedInstanceState)
         setPixelField()
 
@@ -230,7 +41,177 @@ class MainActivity : SystemUiHidingFragmentActivity(R.layout.activity_main) {
         }
     }
 
+    private lateinit var flowFieldPApplet: FlowFieldPApplet
+
+    private fun setPixelField() {
+        if (!::flowFieldPApplet.isInitialized)
+            flowFieldPApplet = FlowFieldPApplet(
+                screenResolution(windowManager)
+            ).also {
+                Timber.i("Initialized flowFieldPApplet")
+            }
+
+        PFragment(flowFieldPApplet).setView(
+            findViewById<FrameLayout>(R.id.canvas_container), this
+        )
+    }
+
+    private fun setButtonOnClickListeners() {
+        // image selection button
+        image_selection_button.setOnClickListener {
+            if (!permissionsHandler.allPermissionsGranted)
+                permissionsHandler.request()
+            else
+                selectImages()
+        }
+
+        // menu button
+        menu_button.setOnClickListener {
+
+            val menuItemToPreferenceKey: Map<Int, String> = mapOf(
+                R.id.main_menu_item_delete_input_screenshots to UserPreferences.Keys.deleteInputScreenshots,
+                R.id.main_menu_item_save_to_autocropped_dir to UserPreferences.Keys.saveToAutocroppedDir,
+                R.id.main_menu_conduct_auto_scrolling to UserPreferences.Keys.conductAutoScrolling
+            )
+
+            // inflate popup menu
+            PopupMenu(this, it).run {
+                menuInflater.inflate(R.menu.activity_main, menu)
+
+                // set checks
+                menuItemToPreferenceKey.entries.forEach { (key, value) ->
+                    menu.findItem(key).isChecked = UserPreferences[value]!!
+                }
+
+                // set item onClickListeners
+                setOnMenuItemClickListener { item ->
+                    item.run {
+                        UserPreferences.toggle(menuItemToPreferenceKey[itemId]!!)
+                        isChecked = !isChecked
+                        persistMenuAfterItemClick(this)
+                    }
+                }
+                show()
+            }
+        }
+
+        // screenshot button
+        screenshot_button.setOnClickListener {
+            flowFieldPApplet.canvas.save(
+                File(
+                    picturesDirectoryPath,
+                    "AutoCrop${formattedDateTimeString()}.jpg"
+                ).absolutePath
+                    .also { Timber.i("Saving flowfield canvas to $it") }
+            )
+
+            displayToast(
+                "Saved Flowfield Image to\n${picturesDirectoryPath}",
+                TextColors.successfullyCarriedOut,
+                Toast.LENGTH_SHORT
+            )
+        }
+    }
+
+    private fun displaySavingResultSnackbar(nSavedCrops: Int) {
+        val textColor: Int = TextColors.successfullyCarriedOut
+
+        with(UserPreferences.deleteInputScreenshots) {
+            when (nSavedCrops) {
+                0 -> displaySnackbar("Dismissed all crops", textColor)
+                1 -> displaySnackbar(
+                    listOf(
+                        "Saved 1 crop",
+                        "Saved 1 crop and deleted\ncorresponding screenshot"
+                    ).getByBoolean(this),
+                    textColor
+                )
+                in 2..Int.MAX_VALUE -> displaySnackbar(
+                    listOf(
+                        "Saved $nSavedCrops crops",
+                        "Saved $nSavedCrops crops and deleted\ncorresponding screenshots"
+                    ).getByBoolean(this),
+                    textColor
+                )
+                else -> Unit
+            }
+        }
+    }
+
     val retrieveSnackbarArgument = SnackbarArgumentRetriever()
+
+    // -----------------Permissions---------------------
+
+    private inner class PermissionsHandler{
+        private val requiredPermissions: List<String> = listOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+
+        val allPermissionsGranted: Boolean
+            get() = permissionToIsGranted.values.all { it }
+
+        private val permissionToIsGranted: MutableMap<String, Boolean> by lazy {
+            requiredPermissions
+                .associateWith { permissionGranted(it) }
+                .toMutableMap()
+        }
+
+        fun request() {
+            val dummyRequestCode: Int = -1
+
+            requestPermissions(
+                requiredPermissions
+                    .filter { permissionToIsGranted[it] == false }
+                    .toTypedArray(),
+                dummyRequestCode
+            )
+        }
+
+        fun onRequestPermissionsResult(
+            permissions: Array<out String>,
+            grantResults: IntArray) {
+
+            val permissionToDescription: Map<String, String> = listOf("writing", "reading")
+                .mapIndexed { index, s -> requiredPermissions[index] to s }
+                .toMap()
+
+            if (grantResults.all { it == PackageManager.PERMISSION_DENIED })
+                return displayToast(
+                    "You need to permit file reading and\n" +
+                            "writing in order for the app to work"
+                )
+
+            (permissions zip grantResults.toTypedArray()).forEach {
+                if (it.second != PackageManager.PERMISSION_GRANTED)
+                    displayToast(
+                        "You need to permit file ${permissionToDescription[it.first]}\n" +
+                                "in order for the app to work"
+                    )
+                else
+                    permissionToIsGranted[it.first] = true
+            }
+
+            if (allPermissionsGranted)
+                selectImages()
+        }
+    }
+
+    private val permissionsHandler = PermissionsHandler()
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        permissionsHandler.onRequestPermissionsResult(permissions, grantResults)
+    }
+
+    // -----------------ImageSelection---------------------
+
+    private enum class IntentCode {
+        IMAGE_SELECTION
+    }
 
     private fun selectImages() {
         Intent(Intent.ACTION_PICK).run {
@@ -249,30 +230,30 @@ class MainActivity : SystemUiHidingFragmentActivity(R.layout.activity_main) {
         if (resultCode == RESULT_OK) {
             when (requestCode) {
                 IntentCode.IMAGE_SELECTION.ordinal -> {
-
-                    fun startCroppingActivity(imageUriStrings: Array<String>) {
-                        startActivity(
-                            Intent(this, CroppingActivity::class.java)
-                                .putExtra(
-                                    SELECTED_IMAGE_URI_STRINGS_IDENTIFIER,
-                                    imageUriStrings
-                                )
-                        ).also {
-                            proceedTransitionAnimation()
-                            onExit()
-                        }
-                    }
-
-                    with(data?.clipData) {
+                    with(data?.clipData!!) {
                         startCroppingActivity(
-                            imageUriStrings = (0 until this?.itemCount!!).map {
-                                getItemAt(it)?.uri!!.toString()
-                            }.toTypedArray()
+                            imageUriStrings = (0 until this.itemCount)
+                                .map { getItemAt(it)?.uri!!.toString() }
+                                .toTypedArray()
                         )
                     }
                 }
             }
         }
+    }
+
+    // -----------------Exiting---------------------
+
+    private fun startCroppingActivity(imageUriStrings: Array<String>) {
+        startActivity(
+            Intent(this, CroppingActivity::class.java)
+                .putExtra(
+                    SELECTED_IMAGE_URI_STRINGS_IDENTIFIER,
+                    imageUriStrings
+                )
+        )
+        proceedTransitionAnimation()
+        onExit()
     }
 
     /**
@@ -281,8 +262,6 @@ class MainActivity : SystemUiHidingFragmentActivity(R.layout.activity_main) {
     override fun onBackPressed() {
         finishAffinity()
     }
-
-    private lateinit var userPreferencesOnActivityCreation: List<Boolean>
 
     /**
      * Writes set preferences to shared preferences
@@ -294,4 +273,6 @@ class MainActivity : SystemUiHidingFragmentActivity(R.layout.activity_main) {
             getDefaultSharedPreferences()
         )
     }
+
+    private lateinit var userPreferencesOnActivityCreation: List<Boolean>
 }
