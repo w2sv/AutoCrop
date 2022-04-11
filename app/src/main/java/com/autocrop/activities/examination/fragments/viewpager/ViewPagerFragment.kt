8cde367813a30565fdc650de8e67d111fcf98dde
w2sv <zangenbergjanek@googlemail.com>
2022-04-11 17:03:55 +0200

    Restructure

--- app/src/main/java/com/autocrop/activities/examination/fragments/viewpager/ViewPagerFragment.kt
+++ app/src/main/java/com/autocrop/activities/examination/fragments/viewpager/ViewPagerFragment.kt
@@ -1,13 +1,9 @@
 package com.autocrop.activities.examination.fragments.viewpager
 
-import android.app.AlertDialog
-import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
-import com.autocrop.global.BooleanUserPreferences
 import com.autocrop.activities.examination.fragments.ExaminationActivityFragment
-import com.autocrop.utils.android.ExtendedDialogFragment
 import com.w2sv.autocrop.databinding.ActivityExaminationFragmentViewpagerBinding
 
 class ViewPagerFragment
@@ -55,45 +51,4 @@ class ViewPagerFragment
             }
         }
     }
-}
-
-abstract class CropEntiretyButtonDialogFragment
-    : ExtendedDialogFragment(){
-
-    val resultKey: String = "${this::class.java.name}_CONFIRMED"
-
-    protected fun setFragmentResult() = requireActivity().supportFragmentManager.setFragmentResult(
-        resultKey,
-        Bundle.EMPTY
-    )
-}
-
-class SaveAllConfirmationDialogFragment
-    : CropEntiretyButtonDialogFragment(){
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
-        AlertDialog.Builder(requireContext())
-            .run {
-                setTitle("Save all crops?")
-                setMultiChoiceItems(arrayOf("Delete corresponding screenshots"), booleanArrayOf(
-                    BooleanUserPreferences.deleteScreenshotsOnSaveAll)){ _, _, _ -> BooleanUserPreferences.toggle(
-                    BooleanUserPreferences.Keys.deleteScreenshotsOnSaveAll)}
-
-                setNegativeButton("No") { _, _ -> }
-                setPositiveButton("Yes") { _, _ -> setFragmentResult()}
-            }
-            .create()
-}
-
-class DiscardAllConfirmationDialogFragment
-    : CropEntiretyButtonDialogFragment(){
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
-        AlertDialog.Builder(requireContext())
-            .run {
-                setTitle("Discard all crops?")
-                setNegativeButton("No") { _, _ -> }
-                setPositiveButton("Yes") { _, _ -> setFragmentResult() }
-            }
-            .create()
 }
