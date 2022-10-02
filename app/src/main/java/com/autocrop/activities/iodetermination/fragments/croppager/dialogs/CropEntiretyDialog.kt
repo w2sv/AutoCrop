90dad8d864b0adb3966c39979275e3cf76667d06
w2sv <zangenbergjanek@googlemail.com>
2022-10-02 19:03:00 +0200

    Refine UI

--- app/src/main/java/com/autocrop/activities/iodetermination/fragments/croppager/dialogs/CropEntiretyDialog.kt
+++ app/src/main/java/com/autocrop/activities/iodetermination/fragments/croppager/dialogs/CropEntiretyDialog.kt
@@ -3,17 +3,24 @@ package com.autocrop.activities.iodetermination.fragments.croppager.dialogs
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.text.SpannableStringBuilder
 import androidx.core.os.bundleOf
+import androidx.core.text.italic
 
 class CropEntiretyDialog: AbstractCropDialog(){
     companion object{
-        const val RESULT_REQUEST_KEY = "CropEntiretyDialog_RESULT_REQUEST_KEY"
+        const val RESULT_REQUEST_KEY = "CROP_ENTIRETY_DIALOG_RESULT_REQUEST_KEY"
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
         AlertDialog.Builder(requireContext())
             .apply {
-                setTitle("Save all crops?")
+                setTitle(
+                    SpannableStringBuilder()
+                        .append("Save ")
+                        .italic { append("all ") }
+                        .append("crops?")
+                )
                 setDeleteCorrespondingScreenshotsOption("Delete corresponding screenshots")
                 setNegativeButton("No, discard all") { _, _ -> setFragmentResult(false)}
                 setPositiveButton("Yes") { _, _ -> setFragmentResult(true)}
