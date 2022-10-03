abbbf910c5195f07cfb14c5f091c0ff7898dc19b
w2sv <zangenbergjanek@googlemail.com>
2022-10-03 18:17:52 +0200

    Remove kotlin-reflect

--- app/src/main/java/com/autocrop/AutoCrop.kt
+++ app/src/main/java/com/autocrop/AutoCrop.kt
@@ -1,7 +1,8 @@
 package com.autocrop
 
 import android.app.Application
-import com.autocrop.preferences.TypedPreferences
+import com.autocrop.preferences.BooleanPreferences
+import com.autocrop.preferences.UriPreferences
 import com.autocrop.utils.android.extensions.getApplicationWideSharedPreferences
 import com.w2sv.autocrop.BuildConfig
 import timber.log.Timber
@@ -10,7 +11,7 @@ class AutoCrop: Application() {
 
     /**
      * Plants [Timber] tree if applicable
-     * initializes [TypedPreferences] child classes from application-wide SharedPreferences
+     * initializes TypedPreferences child classes from application-wide SharedPreferences
      */
     override fun onCreate() {
         super.onCreate()
@@ -19,8 +20,8 @@ class AutoCrop: Application() {
             Timber.plant(Timber.DebugTree())
 
         with(getApplicationWideSharedPreferences()){
-            TypedPreferences::class.sealedSubclasses.forEach {
-                it.objectInstance!!.initializeFromSharedPreferences(this)
+            listOf(BooleanPreferences, UriPreferences).forEach {
+                it.initializeFromSharedPreferences(this)
             }
         }
     }
