a0ef2c02c3445913abd189ef0d92bd24ddee8b15
w2sv <zangenbergjanek@googlemail.com>
2022-10-11 14:36:29 +0200

    BindingAdministrator

---
+++ app/src/main/java/com/autocrop/screencapturelistening/BindingAdministrator.kt
@@ -0,0 +1,42 @@
+package com.autocrop.screencapturelistening
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
+import com.autocrop.screencapturelistening.abstractservices.BoundService
+import com.autocrop.utils.kotlin.delegates.Consumable
+import timber.log.Timber
+
+class BindingAdministrator<T : BoundService>(
+    context: Context,
+    private val boundServiceClass: Class<T>) : ContextWrapper(context) {
+    private var boundService: T? = null
+    private var onServiceConnected by Consumable<(T) -> Unit>(null)
+
+    val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Timber.i("$name connected")
+            boundService = (service as BoundService.LocalBinder).getService()
+            onServiceConnected?.invoke(boundService!!)
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            boundService = null
+        }
+    }
+
+    fun callOnBoundService(function: (T) -> Unit) {
+        if (boundService == null) {
+            onServiceConnected = function
+            bindService(
+                Intent(this, boundServiceClass),
+                serviceConnection,
+                BIND_AUTO_CREATE
+            )
+        } else
+            function(boundService!!)
+    }
+}
--- app/src/main/java/com/autocrop/screencapturelistening/NotificationCancellationService.kt
+++ app/src/main/java/com/autocrop/screencapturelistening/NotificationCancellationService.kt
@@ -1,68 +1,43 @@
 package com.autocrop.screencapturelistening
 
-import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
-import android.os.IBinder
+import com.autocrop.screencapturelistening.abstractservices.UnboundService
 import com.autocrop.screencapturelistening.notification.ASSOCIATED_NOTIFICATION_ID
 import com.autocrop.screencapturelistening.notification.CANCEL_NOTIFICATION_ACTION
-import com.autocrop.screencapturelistening.abstractservices.BoundService
-import com.autocrop.screencapturelistening.abstractservices.UnboundService
 import com.autocrop.utils.android.extensions.notificationManager
-import com.autocrop.utils.kotlin.delegates.Consumable
 import timber.log.Timber
 
-class NotificationCancellationService: UnboundService(){
+class NotificationCancellationService : UnboundService() {
+    private val bindingAdministrator = BindingAdministrator(
+        this,
+        ScreenCaptureListeningService::class.java
+    )
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         PendingIntentRequestCode.notificationCancellationService.remove(startId)
 
-        val notificationId = intent!!.getIntExtra(ASSOCIATED_NOTIFICATION_ID, -1)
-        intent.action?.let {
-            Timber.i("Found action $it")
-            if (it == CANCEL_NOTIFICATION_ACTION)
-                notificationManager().cancel(notificationId)
-                    .also {Timber.i("Cancelled notification $notificationId")}
-        }
+        val notificationId = intent!!.process()
 
-        if (screenCaptureListeningService == null)
-            bindService(
-                Intent(this, ScreenCaptureListeningService::class.java),
-                serviceConnection.apply {
-                    impendingRemoveId = notificationId
-                },
-                Context.BIND_AUTO_CREATE
-            )
-        else
-            screenCaptureListeningService!!.notificationGroup.onChildNotificationCancelled(notificationId)
+        bindingAdministrator.callOnBoundService {
+            it.notificationGroup.onChildNotificationCancelled(notificationId)
+        }
 
         return START_REDELIVER_INTENT
     }
 
-    private var screenCaptureListeningService: ScreenCaptureListeningService? = null
-
-    private val serviceConnection = object: ServiceConnection{
-        var impendingRemoveId by Consumable<Int>(null)
-
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            Timber.i("$name connected")
-            screenCaptureListeningService = (service as? BoundService.LocalBinder)!!
-                .getService<ScreenCaptureListeningService>()
-                .apply {
-                    impendingRemoveId?.let {
-                        notificationGroup.onChildNotificationCancelled(it)
-                    }
-                }
+    private fun Intent.process(): Int{
+        val notificationId = getIntExtra(ASSOCIATED_NOTIFICATION_ID, -1)
+        action?.let {
+            if (it == CANCEL_NOTIFICATION_ACTION)
+                notificationManager().cancel(notificationId)
+                    .also { Timber.i("Cancelled notification $notificationId") }
         }
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-            screenCaptureListeningService = null
-        }
+        return notificationId
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        unbindService(serviceConnection)
+        unbindService(bindingAdministrator.serviceConnection)
     }
 }
