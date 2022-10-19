ca51b718233a03038d8aec0bb88a7a2d98d8d42a
w2sv <zangenbergjanek@googlemail.com>
2022-10-19 18:12:50 +0200

    difference reduction

--- app/src/main/java/com/autocrop/activities/main/fragments/flowfield/sketch/FlowFieldSketch.java
+++ app/src/main/java/com/autocrop/activities/main/fragments/flowfield/sketch/FlowFieldSketch.java
@@ -20,7 +20,7 @@ public class FlowFieldSketch extends PApplet {
     }
 
     private FlowField flowfield;
-    private ArrayList<Particle> particles;
+    private final ArrayList<Particle> particles = new ArrayList<>();
 
     public void setup() {
         background(0);
@@ -30,12 +30,35 @@ public class FlowFieldSketch extends PApplet {
         flowfield.update();
 
         // initialize particles
-        particles = new ArrayList<>();
-
         Particle.staticInitialization(width, height);
 
         for (int i = 0; i < 800; i++)
             particles.add(new Particle());
+    }
+
+    private void alphaDrop(){
+        final int REDUCE_BY = 4;
+
+        g.loadPixels();
+        for (int i = 0; i < g.pixels.length; i++) {
+            int pixel = g.pixels[i];
+            g.pixels[i] = color(
+                max(red(pixel) - REDUCE_BY, 0),
+                max(green(pixel) - REDUCE_BY, 0),
+                max(blue(pixel) - REDUCE_BY, 0),
+                255
+            );
+        }
+        g.updatePixels();
+    }
+
+    int lastAlphaDrop = 0;
+    void dropAlphaIfAppropriate(){
+        int second = second();
+        if (second != lastAlphaDrop && second % 2 == 0) {
+            alphaDrop();
+            lastAlphaDrop = second;
+        }
     }
 
     public void draw(){
@@ -43,6 +66,7 @@ public class FlowFieldSketch extends PApplet {
 
         flowfield.update();
 
+        dropAlphaIfAppropriate();
         for (Particle p : particles) {
             flowfield.affect(p);
             p.update();
