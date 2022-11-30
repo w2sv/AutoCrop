71331086acd1299d2898aca40b529bbfa0035361
w2sv <zangenbergjanek@googlemail.com>
2022-11-30 14:45:26 +0100

    Change DeleteRequestActivity layout

--- app/src/main/kotlin/com/w2sv/autocrop/screenshotlistening/services/DeleteRequestActivity.kt
+++ app/src/main/kotlin/com/w2sv/autocrop/screenshotlistening/services/DeleteRequestActivity.kt
@@ -3,14 +3,15 @@ package com.w2sv.autocrop.screenshotlistening.services
 import android.app.Activity
 import android.os.Build
 import android.provider.MediaStore
+import androidx.activity.ComponentActivity
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import com.w2sv.autocrop.activities.BlankViewBoundActivity
+import com.w2sv.autocrop.R
 import com.w2sv.autocrop.utils.extensions.getParcelable
 
 @RequiresApi(Build.VERSION_CODES.R)
-class DeleteRequestActivity : BlankViewBoundActivity() {
+class DeleteRequestActivity : ComponentActivity(R.layout.fragment_delete_request) {
     companion object {
         const val EXTRA_CONFIRMED_DELETION = "com.w2sv.autocrop.CONFIRMED_DELETION"
     }
