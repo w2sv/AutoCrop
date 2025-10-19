4c19203f335ece88ec94c454a45b4921badf1a6c
w2sv <zangenbergjanek@googlemail.com>
2025-10-19 21:37:40 +0200

    Structure

--- app/src/main/kotlin/com/w2sv/autocrop/ui/screen/cropadjustment/CropAdjustmentFragment.kt
+++ app/src/main/kotlin/com/w2sv/autocrop/ui/screen/cropadjustment/CropAdjustmentFragment.kt
@@ -1,45 +1,14 @@
 package com.w2sv.autocrop.ui.screen.cropadjustment
 
 import android.content.Context
-import android.graphics.Bitmap
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsIgnoringVisibility
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButtonDefaults
-import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.graphics.createBitmap
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.w2sv.autocrop.R
 import com.w2sv.autocrop.ui.ComposeAppFragment
 import com.w2sv.autocrop.ui.screen.cropSessionInjectedViewModel
-import com.w2sv.autocrop.ui.screen.cropadjustment.model.AdjustmentModeState
-import com.w2sv.autocrop.ui.screen.cropadjustment.model.AdjustmentViewState
-import com.w2sv.autocrop.ui.screen.cropadjustment.view.CropAdjustmentView
-import com.w2sv.autocrop.ui.theme.AppTheme
 import com.w2sv.autocrop.ui.util.hideNavigationBar
 import com.w2sv.autocrop.ui.util.showSystemBars
-import com.w2sv.domain.model.CropEdges
 import dagger.hilt.android.AndroidEntryPoint
-import slimber.log.i
 
 @AndroidEntryPoint
 class CropAdjustmentFragment : ComposeAppFragment() {
@@ -59,7 +28,7 @@ class CropAdjustmentFragment : ComposeAppFragment() {
     @Composable
     override fun ScreenContent() {
         val viewState by viewModel.viewState.collectAsStateWithLifecycle()
-        AdjustmentScreen(
+        CropAdjustmentScreen(
             state = viewState,
             image = viewModel.screenshotBitmap,
             onModeStateChanged = viewModel::updateAdjustmentModeState,
@@ -72,160 +41,3 @@ class CropAdjustmentFragment : ComposeAppFragment() {
         )
     }
 }
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun AdjustmentScreen(
-    state: AdjustmentViewState,
-    image: Bitmap,
-    onModeStateChanged: (AdjustmentModeState) -> Unit,
-    onReset: () -> Unit,
-    onApply: () -> Unit,
-    onBack: () -> Unit
-) {
-    Scaffold(contentWindowInsets = WindowInsets.statusBarsIgnoringVisibility) { paddingValues ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
-            CropAdjustmentView(
-                state = state,
-                image = image,
-                onModeStateChanged = onModeStateChanged
-            )
-            TopButtons(
-                cropHasBeenAdjusted = state.adjustmentCanBeApplied,
-                onReset = onReset,
-                onApply = onApply,
-                onBack = onBack
-            )
-        }
-    }
-}
-
-@Composable
-private fun BoxScope.TopButtons(
-    cropHasBeenAdjusted: Boolean,
-    onReset: () -> Unit,
-    onApply: () -> Unit,
-    onBack: () -> Unit
-) {
-    val modifier = Modifier.padding(top = 24.dp)
-    val filledIconButtonColors =
-        IconButtonDefaults.filledIconButtonColors(containerColor = colorScheme.surfaceContainerHighest.copy(alpha = 0.5f))
-
-    FilledIconButton(
-        onClick = onBack,
-        modifier = modifier
-            .padding(start = 8.dp)
-            .size(48.dp),
-        colors = filledIconButtonColors
-    ) {
-        Icon(painterResource(R.drawable.ic_arrow_back_24), contentDescription = null)
-    }
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = Modifier
-            .align(Alignment.TopEnd)
-            .then(modifier)
-            .padding(end = 12.dp)
-    ) {
-        FilledIconButton(
-            onClick = onReset,
-            enabled = cropHasBeenAdjusted,
-            colors = filledIconButtonColors,
-            modifier = Modifier.size(48.dp)
-        ) {
-            Icon(
-                painterResource(R.drawable.ic_restart_24),
-                contentDescription = null
-            )
-        }
-        FilledIconButton(
-            onClick = onApply,
-            enabled = cropHasBeenAdjusted,
-            colors = filledIconButtonColors.copy(contentColor = Color.Green),
-            modifier = Modifier.size(48.dp)
-        ) {
-            Icon(
-                painterResource(R.drawable.ic_check_24),
-                contentDescription = null
-            )
-        }
-    }
-}
-
-@Composable
-private fun CropAdjustmentView(
-    state: AdjustmentViewState,
-    image: Bitmap,
-    onModeStateChanged: (AdjustmentModeState) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    AndroidView(
-        modifier = modifier,
-        factory = {
-            i { "Running factory" }
-            CropAdjustmentView(it).apply {
-                initialize(
-                    image = image,
-                    cropEdges = state.adjustedEdges
-                        ?: state.originalEdges
-                )
-                adjustmentModeStateChangedListener = onModeStateChanged
-            }
-        },
-        update = { view ->
-            state.adjustedEdges?.let { view.updateFromEdges(it) }
-        }
-    )
-}
-
-@Preview
-@Composable
-private fun Prev() {
-    val originalEdges = CropEdges(200, 1200)
-
-    AppTheme {
-        AdjustmentScreen(
-            AdjustmentViewState(
-                originalEdges = originalEdges,
-                modeState = AdjustmentModeState.Manual(CropEdges(400, 1200))
-            ),
-            createBitmap(800, 1600),
-            {},
-            {},
-            {},
-            {}
-        )
-    }
-}
-
-// @Composable
-// private fun ModeSelectionButtons(
-//    selectedMode: CropAdjustmentMode,
-//    onModeSelected: (CropAdjustmentMode) -> Unit,
-//    modifier: Modifier = Modifier
-// ) {
-//    SingleChoiceSegmentedButtonRow(modifier) {
-//        CropAdjustmentMode.entries.forEachIndexed { i, mode ->
-//            val isSelected = remember(selectedMode) { mode == selectedMode }
-//            SegmentedButton(
-//                selected = isSelected,
-//                onClick = {
-//                    if (!isSelected) {
-//                        onModeSelected(mode)
-//                    }
-//                },
-//                shape = SegmentedButtonDefaults.itemShape(
-//                    index = i,
-//                    count = 2
-//                )
-//            ) {
-//                Text(text = stringResource(id = mode.labelRes))
-//            }
-//        }
-//    }
-// }
---
+++ app/src/main/kotlin/com/w2sv/autocrop/ui/screen/cropadjustment/CropAdjustmentScreen.kt
@@ -0,0 +1,187 @@
+package com.w2sv.autocrop.ui.screen.cropadjustment
+
+import android.graphics.Bitmap
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsIgnoringVisibility
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.graphics.createBitmap
+import com.w2sv.autocrop.R
+import com.w2sv.autocrop.ui.screen.cropadjustment.model.AdjustmentModeState
+import com.w2sv.autocrop.ui.screen.cropadjustment.model.AdjustmentViewState
+import com.w2sv.autocrop.ui.screen.cropadjustment.view.CropAdjustmentView
+import com.w2sv.autocrop.ui.theme.AppTheme
+import com.w2sv.autocrop.ui.util.compose.debounceClick
+import com.w2sv.domain.model.CropEdges
+
+@Composable
+fun CropAdjustmentScreen(
+    state: AdjustmentViewState,
+    image: Bitmap,
+    onModeStateChanged: (AdjustmentModeState) -> Unit,
+    onReset: () -> Unit,
+    onApply: () -> Unit,
+    onBack: () -> Unit
+) {
+    Scaffold(contentWindowInsets = WindowInsets.statusBarsIgnoringVisibility) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            CropAdjustmentView(
+                state = state,
+                image = image,
+                onModeStateChanged = onModeStateChanged,
+                modifier = Modifier.padding(horizontal = 22.dp)
+            )
+            TopButtons(
+                cropHasBeenAdjusted = state.adjustmentCanBeApplied,
+                onReset = onReset,
+                onApply = onApply,
+                onBack = onBack
+            )
+        }
+    }
+}
+
+@Composable
+private fun BoxScope.TopButtons(
+    cropHasBeenAdjusted: Boolean,
+    onReset: () -> Unit,
+    onApply: () -> Unit,
+    onBack: () -> Unit
+) {
+    val modifier = Modifier.padding(top = 24.dp)
+    val filledIconButtonColors =
+        IconButtonDefaults.filledIconButtonColors(containerColor = colorScheme.surfaceContainerHighest.copy(alpha = 0.5f))
+
+    FilledIconButton(
+        onClick = debounceClick(onClick = onBack),
+        modifier = modifier
+            .padding(start = 8.dp)
+            .size(48.dp),
+        colors = filledIconButtonColors
+    ) {
+        Icon(painterResource(R.drawable.ic_arrow_back_24), contentDescription = null)
+    }
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier
+            .align(Alignment.TopEnd)
+            .then(modifier)
+            .padding(end = 12.dp)
+    ) {
+        FilledIconButton(
+            onClick = debounceClick(onClick = onReset),
+            enabled = cropHasBeenAdjusted,
+            colors = filledIconButtonColors,
+            modifier = Modifier.size(48.dp)
+        ) {
+            Icon(
+                painterResource(R.drawable.ic_restart_24),
+                contentDescription = null
+            )
+        }
+        FilledIconButton(
+            onClick = debounceClick(onClick = onApply),
+            enabled = cropHasBeenAdjusted,
+            colors = filledIconButtonColors.copy(contentColor = Color.Green),
+            modifier = Modifier.size(48.dp)
+        ) {
+            Icon(
+                painterResource(R.drawable.ic_check_24),
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Composable
+private fun CropAdjustmentView(
+    state: AdjustmentViewState,
+    image: Bitmap,
+    onModeStateChanged: (AdjustmentModeState) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AndroidView(
+        modifier = modifier,
+        factory = {
+            CropAdjustmentView(it).apply {
+                initialize(
+                    image = image,
+                    cropEdges = state.adjustedEdges
+                        ?: state.originalEdges
+                )
+                adjustmentModeStateChangedListener = onModeStateChanged
+            }
+        },
+        update = { view -> state.adjustedEdges?.let { view.updateFromEdges(it) } }
+    )
+}
+
+@Preview
+@Composable
+private fun Prev() {
+    val originalEdges = CropEdges(200, 1200)
+
+    AppTheme {
+        CropAdjustmentScreen(
+            AdjustmentViewState(
+                originalEdges = originalEdges,
+                modeState = AdjustmentModeState.Manual(CropEdges(400, 1200))
+            ),
+            createBitmap(800, 1600),
+            {},
+            {},
+            {},
+            {}
+        )
+    }
+}
+
+// @Composable
+// private fun ModeSelectionButtons(
+//    selectedMode: CropAdjustmentMode,
+//    onModeSelected: (CropAdjustmentMode) -> Unit,
+//    modifier: Modifier = Modifier
+// ) {
+//    SingleChoiceSegmentedButtonRow(modifier) {
+//        CropAdjustmentMode.entries.forEachIndexed { i, mode ->
+//            val isSelected = remember(selectedMode) { mode == selectedMode }
+//            SegmentedButton(
+//                selected = isSelected,
+//                onClick = {
+//                    if (!isSelected) {
+//                        onModeSelected(mode)
+//                    }
+//                },
+//                shape = SegmentedButtonDefaults.itemShape(
+//                    index = i,
+//                    count = 2
+//                )
+//            ) {
+//                Text(text = stringResource(id = mode.labelRes))
+//            }
+//        }
+//    }
+// }
