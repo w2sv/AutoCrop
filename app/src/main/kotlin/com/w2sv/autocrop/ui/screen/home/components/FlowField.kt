af14b3c9f876c7b36164fc19bba5ccd29fe2f5ac
w2sv <zangenbergjanek@googlemail.com>
2025-11-01 18:56:54 +0100

    Fix ProcessingSketch restarting on config change

--- app/src/main/kotlin/com/w2sv/autocrop/ui/screen/home/components/FlowField.kt
+++ app/src/main/kotlin/com/w2sv/autocrop/ui/screen/home/components/FlowField.kt
@@ -1,7 +1,5 @@
 package com.w2sv.autocrop.ui.screen.home.components
 
-import android.view.View
-import android.widget.FrameLayout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
@@ -9,13 +7,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalInspectionMode
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.fragment.app.FragmentActivity
-import com.w2sv.autocrop.ui.util.findActivity
+import com.w2sv.autocrop.ui.util.compose.ProcessingSketch
 import com.w2sv.autocrop.ui.util.rememberScreenResolution
 import com.w2sv.flowfield.PerlinNoiseFlowFieldSketch
-import processing.android.PFragment
-import processing.core.PApplet
 
 @Composable
 fun FlowFieldOrPreviewMock(modifier: Modifier = Modifier) {
@@ -24,7 +18,8 @@ fun FlowFieldOrPreviewMock(modifier: Modifier = Modifier) {
             modifier
                 .background(Color(145, 25, 69))
         )
-    } else {
+    }
+    else {
         FlowField(modifier)
     }
 }
@@ -32,24 +27,6 @@ fun FlowFieldOrPreviewMock(modifier: Modifier = Modifier) {
 @Composable
 private fun FlowField(modifier: Modifier = Modifier) {
     val screenResolution = rememberScreenResolution()
-    val sketch = remember(screenResolution) { PerlinNoiseFlowFieldSketch(screenResolution) }
+    val sketch = remember(screenResolution) { PerlinNoiseFlowFieldSketch(screenResolution.x, screenResolution.y) }
     ProcessingSketch(sketch, modifier)
-}
-
-@Composable
-private fun ProcessingSketch(sketch: PApplet, modifier: Modifier = Modifier) {
-    AndroidView(
-        factory = { context ->
-            FrameLayout(context).apply {
-                id = View.generateViewId()
-                PFragment(sketch)
-                    .setView(
-                        this,
-                        context.findActivity() as? FragmentActivity
-                            ?: throw IllegalStateException("FragmentActivity not found for ProcessingSketch")
-                    )
-            }
-        },
-        modifier = modifier
-    )
 }
--- core/flowfield/src/main/java/com/w2sv/flowfield/PerlinNoiseFlowFieldSketch.java
+++ core/flowfield/src/main/java/com/w2sv/flowfield/PerlinNoiseFlowFieldSketch.java
@@ -1,10 +1,8 @@
-/*
- * Core graphical algorithm by Daniel Shiffman @ https://thecodingtrain.com/CodingChallenges/024-perlinnoiseflowfield.html
+/**
+ * Core algorithm by Daniel Shiffman @ https://thecodingtrain.com/CodingChallenges/024-perlinnoiseflowfield.html
  */
 
 package com.w2sv.flowfield;
-
-import android.graphics.Point;
 
 import com.google.common.collect.Sets;
 
@@ -21,9 +19,9 @@ public class PerlinNoiseFlowFieldSketch extends PApplet {
     private final AlphaDropper alphaDropper = new AlphaDropper();
     private final ColorHandler colorHandler = new ColorHandler();
 
-    public PerlinNoiseFlowFieldSketch(Point screenResolution) {
-        width = screenResolution.x;
-        height = screenResolution.y;
+    public PerlinNoiseFlowFieldSketch(int width, int height) {
+        this.width = width;
+        this.height = height;
     }
 
     @Override
@@ -44,8 +42,9 @@ public class PerlinNoiseFlowFieldSketch extends PApplet {
         // initialize particles
         Particle.setFlowFieldDimensions(width, height);
 
-        for (int i = 0; i < Config.N_PARTICLES; i++)
+        for (int i = 0; i < Config.N_PARTICLES; i++) {
             particles.add(new Particle(this));
+        }
     }
 
     @Override
@@ -72,12 +71,12 @@ public class PerlinNoiseFlowFieldSketch extends PApplet {
         static final float PARTICLE_STROKE_ALPHA = 48;
         static final int PARTICLE_STROKE_WEIGHT = 2;
         static final Set<Integer> PARTICLE_COLORS = Set.of(
-                0xFFBC275E,  // magenta bright
-                0xFF911945,  // magenta saturated
-                0xFF701145,  // magenta dark
-                0xFFB00020,  // red
-                0xFF6B13B5,  // purple
-                0xFF1a0ac7   // ocean blue
+            0xFFBC275E,  // magenta bright
+            0xFF911945,  // magenta saturated
+            0xFF701145,  // magenta dark
+            0xFFB00020,  // red
+            0xFF6B13B5,  // purple
+            0xFF1a0ac7   // ocean blue
         );
         static final int BACKGROUND_COLOR = 0;
         static final int FLOW_FIELD_GRANULARITY = 200;
@@ -136,16 +135,15 @@ class AlphaDropper {
             int argb = canvas.pixels[i];
             if (argb != UNSET) {
                 canvas.pixels[i] = UNSET |
-                        attenuatedValue((argb >> 16) & 0xFF) << 16 |
-                        attenuatedValue((argb >> 8) & 0xFF) << 8 |
-                        attenuatedValue(argb & 0xFF);
+                    attenuatedValue((argb >> 16) & 0xFF) << 16 |
+                    attenuatedValue((argb >> 8) & 0xFF) << 8 |
+                    attenuatedValue(argb & 0xFF);
             }
         }
     }
 
     private int attenuatedValue(int value) {
-        if (value == 0)
-            return value;
+        if (value == 0) return value;
         return value >= 32 ? value - (value >> 5) : PApplet.max(value - 2, 0);
     }
 }
